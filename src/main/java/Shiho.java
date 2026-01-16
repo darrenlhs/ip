4c769d7439ce50a37d4dd29ac804b269f879b7ba
Darren Lee <hansheng.darrenlee@gmail.com>
2026-01-16 21:28:25 +0800

    Added a list function that adds each user input to a list and outputs when requested

--- src/main/java/Shiho.java
+++ src/main/java/Shiho.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 
 public class Shiho {
     public static void main(String[] args) {
-        String logo = "Shiho";
+        String[] user_inputs = new String[100];
+        int counter = 0;
 
         System.out.println("Hello, I'm Shiho.\n"
                 + "What do you need?\n");
@@ -14,9 +15,19 @@ public class Shiho {
             if (userReply.equals("bye")) {
                 System.out.println("Bye. Come back soon.");
                 break;
+            } else if (userReply.equals("list")) {
+                for (int i = 0; i < user_inputs.length; i++) {
+                    if (user_inputs[i] == null) {
+                        break;
+                    }
+                    int number = i + 1;
+                    System.out.println(number + ". " + user_inputs[i]);
+                }
+            } else {
+                user_inputs[counter] = userReply;
+                counter++;
+                System.out.println("added: " + userReply);
             }
-
-            System.out.println(userReply);
         }
 
     }
