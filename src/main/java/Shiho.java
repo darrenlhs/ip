8657f43cf2e55ee7e277fbf897491859e8516e3f
Darren Lee <hansheng.darrenlee@gmail.com>
2026-01-16 21:10:30 +0800

    Added scanner system that reads user's input and echoes back to them

--- src/main/java/Shiho.java
+++ src/main/java/Shiho.java
@@ -1,9 +1,27 @@
+import java.util.Scanner;
+
 public class Shiho {
     public static void main(String[] args) {
         String logo = "Shiho";
 
         System.out.println("Hello, I'm Shiho.\n"
-                + "What do you need?\n\n"
-                + "Bye. Come back soon.");
+                + "What do you need?\n");
+
+        while (true) {
+            Scanner myScan = new Scanner(System.in);
+            String userReply = myScan.nextLine();
+
+            if (userReply.equals("bye")) {
+                System.out.println("Bye. Come back soon.");
+                break;
+            }
+
+            System.out.println(userReply);
+        }
+
     }
+
+
+
 }
+
