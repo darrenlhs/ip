c9affaf80e4e33dc01e4260fdc073ee9927512a3
Darren Lee <hansheng.darrenlee@gmail.com>
2026-01-16 20:34:39 +0800

    Changed chatbot name to Shiho and modified the greeting

---
+++ src/main/java/Shiho.java
@@ -0,0 +1,9 @@
+public class Shiho {
+    public static void main(String[] args) {
+        String logo = "Shiho";
+
+        System.out.println("Hello, I'm Shiho.\n"
+                + "What do you need?\n\n"
+                + "Bye. Come back soon.");
+    }
+}
