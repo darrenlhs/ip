48e51866e143e8295e8036ed44765b115da82c0f
Darren Lee <hansheng.darrenlee@gmail.com>
2026-01-19 15:06:56 +0800

    Added the delete functionality, usage of ArrayList to store Tasks, and updated syntax and exception handling for the new and old code

--- src/main/java/Shiho.java
+++ src/main/java/Shiho.java
@@ -1,9 +1,9 @@
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Shiho {
     public static void main(String[] args) {
-        Task[] user_inputs = new Task[100];
+        ArrayList<Task> user_inputs = new ArrayList<>();
         int counter = 0;
 
         System.out.println("Hello, I'm Shiho Hinomori.\n" + "What do you need?\n");
@@ -15,38 +15,49 @@ public class Shiho {
             String[] parts = userReply.split(" ");
 
             if (userReply.equals("bye")) {
+                // bye command ends the current instance
+
                 System.out.println("Bye. Come back soon.");
                 break;
             }
             else if (userReply.equals("list")) {
-                System.out.println("Here are your tasks:\n");
-                for (int i = 0; i < user_inputs.length; i++) {
-                    if (user_inputs[i] == null) {
-                        break;
+                // list command displays current list of tasks
+
+                if (user_inputs.isEmpty()) {
+                    System.out.println("The task list is empty.\n");
+                }
+                else {
+                    System.out.println("Here are your tasks:\n");
+                    for (int i = 0; i < user_inputs.size(); i++) {
+                        int number = i + 1;
+                        System.out.println(number
+                                + "."
+                                + user_inputs.get(i).toString()
+                        );
                     }
-                    int number = i + 1;
-                    System.out.println(number
-                            + "."
-                            + "["
-                            + user_inputs[i].getStatusIcon()
-                            + "] "
-                            + user_inputs[i].description);
                 }
             }
             else if (parts[0].equals("mark")) {
+                // mark command marks a specific task as done
+
                 try {
                     int taskNumber = Integer.parseInt(parts[1]);
-                    user_inputs[taskNumber - 1].markAsDone();
+                    user_inputs.get(taskNumber - 1).markAsDone();
                     System.out.println(
                             "Okay. I've marked this task as done:\n"
                                     + "   ["
-                                    + user_inputs[taskNumber - 1].getStatusIcon()
+                                    + user_inputs.get(taskNumber - 1).getStatusIcon()
                                     + "] "
-                                    + user_inputs[taskNumber - 1].description
+                                    + user_inputs.get(taskNumber - 1).description
                     );
                 }
-                catch (ArrayIndexOutOfBoundsException | NullPointerException e) {
-                    System.out.println("Invalid task index\n");
+                catch (IndexOutOfBoundsException | NullPointerException e) {
+                    if (user_inputs.isEmpty()) {
+                        System.out.println("The task list is empty.\n");
+                    }
+                    else {
+                        System.out.println("Invalid task index\n");
+                    }
                 }
                 catch (NumberFormatException e) {
                     System.out.println("Non-integer task index provided\n");
@@ -55,33 +66,72 @@ public class Shiho {
 
             }
             else if (parts[0].equals("unmark")) {
+                // unmark command marks a specific task as undone
+
                 try {
                     int taskNumber = Integer.parseInt(parts[1]);
-                    user_inputs[taskNumber - 1].markAsUndone();
+                    user_inputs.get(taskNumber - 1).markAsUndone();
                     System.out.println(
                             "Okay. I've marked this task as not done yet:\n"
                                     + "   ["
-                                    + user_inputs[taskNumber - 1].getStatusIcon()
+                                    + user_inputs.get(taskNumber - 1).getStatusIcon()
                                     + "] "
-                                    + user_inputs[taskNumber - 1].description
+                                    + user_inputs.get(taskNumber - 1).description
                     );
                 }
-                catch (ArrayIndexOutOfBoundsException | NullPointerException e) {
-                    System.out.println("Invalid task index\n");
+                catch (IndexOutOfBoundsException | NullPointerException e) {
+                    if (user_inputs.isEmpty()) {
+                        System.out.println("The task list is empty.\n");
+                    }
+                    else {
+                        System.out.println("Invalid task index\n");
+                    }
                 }
                 catch (NumberFormatException e) {
                     System.out.println("Non-integer task index provided\n");
                 }
             }
+            else if (parts[0].equals("delete")) {
+                // delete command removes a specific task from the list
+                try {
+                    int taskNumber = Integer.parseInt(parts[1]);
+                    counter--;
+                    System.out.println(
+                            "Noted. I've removed this task:\n"
+                                    + "   "
+                                    + user_inputs.get(taskNumber - 1).toString()
+                    );
+                    if (counter == 1) {
+                        System.out.println("Now you have 1 task in the list.\n");
+                    } else {
+                        System.out.println("Now you have " + counter + " tasks in the list.\n");
+
+                        user_inputs.remove(taskNumber - 1);
+                    }
+                }
+                catch (IndexOutOfBoundsException | NullPointerException e) {
+                    if (user_inputs.isEmpty()) {
+                        System.out.println("The task list is empty.\n");
+                    } else {
+                        System.out.println("Invalid task index\n");
+                    }
+                }
+                catch (NumberFormatException e) {
+                    System.out.println("Non-integer task index provided\n");
+
+                }
+            }
             else {
+                // todo, deadline and event commands indicate specific types of tasks to be added to the list
+
                 boolean isValid = true;
 
                 if (userReply.startsWith("todo")) {
                     try {
                         String[] todoArr = userReply.split("todo ");
-                        user_inputs[counter] = new ToDo(todoArr[1]);
+                        user_inputs.add(new ToDo(todoArr[1]));
                     }
-                    catch (ArrayIndexOutOfBoundsException e) {
+                    catch (IndexOutOfBoundsException e) {
                         System.out.println(
                                 "Wrong input syntax. Correct syntax is 'todo (task name)'.\n"
                         );
@@ -97,9 +147,9 @@ public class Shiho {
                     try {
                         String[] deadlineArr1 = userReply.split("deadline ");
                         String[] deadlineArr2 = deadlineArr1[1].split(" /by ");
-                        user_inputs[counter] = new Deadline(deadlineArr2[0], deadlineArr2[1]);
+                        user_inputs.add(new Deadline(deadlineArr2[0], deadlineArr2[1]));
                     }
-                    catch (ArrayIndexOutOfBoundsException e) {
+                    catch (IndexOutOfBoundsException e) {
                         System.out.println(
                                 "Wrong input syntax. Correct syntax is 'deadline (task name) /by (deadline)'.\n"
                         );
@@ -118,9 +168,9 @@ public class Shiho {
                         String[] eventArr2 = eventArr1[1].split(" /from ");
                         String[] eventArr3 = eventArr2[1].split(" /to ");
 
-                        user_inputs[counter] = new Event(eventArr2[0], eventArr3[0], eventArr3[1]);
+                        user_inputs.add(new Event(eventArr2[0], eventArr3[0], eventArr3[1]));
                     }
-                    catch (ArrayIndexOutOfBoundsException e) {
+                    catch (IndexOutOfBoundsException e) {
                         System.out.println(
                                 "Wrong input syntax. Correct syntax is 'event (task name) /from (start) /to (end)'.\n"
                         );
@@ -128,13 +178,15 @@ public class Shiho {
                     }
                 }
                 else {
+                    // invalid command or empty input
+
                     System.out.println("Your input is either empty or not recognised. Please try again.\n");
                     isValid = false;
                 }
                 if (isValid) {
                     // only increments and adds to task list if task command is valid
 
-                    System.out.println("\n" + "Got it. I've added this task: " + user_inputs[counter].toString() + "\n");
+                    System.out.println("\n" + "Got it. I've added this task: " + user_inputs.get(counter).toString() + "\n");
                     counter++;
 
                     if (counter == 1) {
@@ -144,8 +196,6 @@ public class Shiho {
                         System.out.println("Now you have " + counter + " tasks in the list.\n");
                     }
                 }
-
-
 
             }
         }
