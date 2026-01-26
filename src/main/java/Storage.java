b33e090f48aa543a0d9e1c52f02b18bad1cfc0b1
Darren Lee <hansheng.darrenlee@gmail.com>
2026-01-26 21:17:19 +0800

    Add Ui, TaskList, Storage, Parser classes, and account for OOP

---
+++ src/main/java/Storage.java
@@ -0,0 +1,45 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+public class Storage {
+
+    private Path filePath;
+
+    public Storage(String filePath) {
+        this.filePath = Paths.get(filePath);
+    }
+
+    public ArrayList<Task> load() throws IOException {
+        // loads the existing task list from the file path, and creates a new one if it doesn't exist
+
+        if (!Files.exists(filePath)) {
+            // if the directory and file does not exist yet, creates it
+            Files.createDirectories(filePath.getParent());
+            Files.createFile(filePath);
+        }
+
+        ArrayList<Task> tasks = new ArrayList<>();
+
+        ArrayList<String> lines = new ArrayList<>(Files.readAllLines(filePath));
+
+        for (String line: lines) {
+            Task t = Parser.parseFileLine(line);
+            tasks.add(t);
+        }
+
+        return tasks;
+    }
+
+    public void save(ArrayList<Task> tasks) throws IOException {
+        try (BufferedWriter writer = Files.newBufferedWriter(filePath)) {
+            for (Task t : tasks) {
+                writer.write(t.toFileString());
+                writer.newLine();
+            }
+        }
+    }
+}
---
+++ src/main/java/TaskList.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+public class TaskList {
+
+    private ArrayList<Task> tasks;
+
+    public TaskList() {
+        tasks = new ArrayList<>();
+    }
+
+    public TaskList(ArrayList<Task> tasks) {
+        this.tasks = tasks;
+    }
+
+    public void add(Task t) {
+        tasks.add(t);
+    }
+
+    public Task get(int index) {
+        return tasks.get(index);
+    }
+
+    public Task remove(int index) {
+        return tasks.remove(index);
+    }
+
+    public boolean isEmpty() {
+        return tasks.isEmpty();
+    }
+
+    public int size() {
+        return tasks.size();
+    }
+
+    public ArrayList<Task> getAll() {
+        return tasks;
+    }
+}
