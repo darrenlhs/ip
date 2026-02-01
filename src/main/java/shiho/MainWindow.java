46728ff1cf7a7e84834104d24859eab329201823
Darren Lee <hansheng.darrenlee@gmail.com>
2026-02-01 17:20:46 +0800

    Add JavaFX components to package and adjust existing code

---
+++ src/main/java/shiho/MainWindow.java
@@ -0,0 +1,76 @@
+package shiho;
+
+import javafx.application.Platform;
+
+import javafx.fxml.FXML;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+
+import javafx.scene.image.Image;
+
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+/**
+ * Controller for the main GUI.
+ */
+public class MainWindow extends AnchorPane {
+    @FXML
+    private ScrollPane scrollPane;
+    @FXML
+    private VBox dialogContainer;
+    @FXML
+    private TextField userInput;
+    @FXML
+    private Button sendButton;
+
+    private Shiho shiho;
+
+    private final Image userImage = new Image(this.getClass().getResourceAsStream("/images/ichika.jpg"));
+    private final Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/shiho.jpeg"));
+
+    @FXML
+    public void initialize() {
+        scrollPane.vvalueProperty().bind(dialogContainer.heightProperty());
+
+        // Shiho's initial greeting to the user
+        dialogContainer.getChildren().add(
+                DialogBox.getDukeDialog(
+                        "Hello, I'm Shiho Hinomori. What do you need?",
+                        dukeImage
+                )
+        );
+    }
+
+    /** Injects the Duke instance */
+    public void setShiho(Shiho s) {
+        shiho = s;
+
+        // check for errors in loading the task list file from storage and display error message
+        if (!shiho.getStartupMessage().isEmpty()) {
+            dialogContainer.getChildren().add(
+                    DialogBox.getDukeDialog(shiho.getStartupMessage(), dukeImage)
+            );
+        }
+    }
+
+    /**
+     * Creates two dialog boxes, one echoing user input and the other containing Duke's reply and then appends them to
+     * the dialog container. Clears the user input after processing.
+     */
+    @FXML
+    private void handleUserInput() {
+        String input = userInput.getText();
+        String response = shiho.getResponse(input);
+        dialogContainer.getChildren().addAll(
+                DialogBox.getUserDialog(input, userImage),
+                DialogBox.getDukeDialog(response, dukeImage)
+        );
+        userInput.clear();
+
+        if (input.equals("bye")) {
+            Platform.exit();
+        }
+    }
+}
--- src/main/java/shiho/Shiho.java
+++ src/main/java/shiho/Shiho.java
@@ -9,255 +9,243 @@ public class Shiho {
 
     private Storage storage;
     private TaskList tasks;
-    private Ui ui;
     private Parser parser;
+    private String startupMessage;
 
     public Shiho(String filePath) {
-        ui = new Ui();
         storage = new Storage(filePath);
         parser = new Parser();
+        startupMessage = "";
 
         try {
             tasks = new TaskList(storage.load());
         } catch (Exception e) {
-            ui.showLoadError();
+            startupMessage = "No existing task list found in storage. Creating empty task list.";
             tasks = new TaskList();
         }
     }
 
     /**
      * Runs the Shiho chatbot and handles inputs depending on return value of the Parser.
+     *
+     * @return String to be sent to the MainWindow class to be printed out
      */
-    public void run() {
-        ui.show("Hello, I'm Shiho Hinomori.");
-        ui.show("What do you need?\n");
+    public String getResponse(String input) {
+        String[] parts = input.split(" ");
+        String response = "";
 
-        while (true) {
-            String input = ui.readCommand();
-            String[] parts = input.split(" ");
+        String parserPhrase = parser.parse(input);
 
-            String parserPhrase = parser.parse(input);
+        // initially assume input is valid (isValid only applies for todo/deadline/event commands)
+        // hence, isValid is set to false for all commands except todo/deadline/event ones
+        boolean isValid = true;
 
-            // initially assume input is valid (isValid only applies for todo/deadline/event commands)
-            boolean isValid = true;
+        switch (parserPhrase) {
+        case "bye":
+            response = "Bye. Come back soon.";
+            isValid = false;
+            break;
+        case "list":
+            if (tasks.isEmpty()) {
+                response = "The task list is empty.\n";
+            } else {
+                response = "Here are your tasks:\n";
+                for (int i = 0; i < tasks.size(); i++) {
+                    int number = i + 1;
+                    response += number
+                            + "."
+                            + tasks.get(i).toString();
 
-            if (parserPhrase.equals("bye")) {
-                ui.show("Bye. Come back soon.");
-                break;
+                    if (i == tasks.size() - 1) {
+                        response +="\n"; // adds a newline if it's the last element of the list
+                    }
+                }
             }
+            isValid = false;
+            break;
 
-            switch (parserPhrase) {
-            case "list":
-                if (tasks.isEmpty()) {
-                    ui.show("The task list is empty.\n");
-                } else {
-                    ui.show("Here are your tasks:\n");
-                    for (int i = 0; i < tasks.size(); i++) {
-                        int number = i + 1;
-                        ui.show(number
-                                + "."
-                                + tasks.get(i).toString()
-                        );
-
-                        if (i == tasks.size() - 1) {
-                            ui.show("\n"); // adds a newline if it's the last element of the list
-                        }
-                    }
-                }
-                isValid = false;
-                break;
-
-            case "mark":
-                try {
-                    int taskNumber = Integer.parseInt(parts[1]);
-                    tasks.get(taskNumber - 1).markAsDone();
-                    ui.show("Okay. I've marked this task as done:\n"
-                            + "   ["
-                            + tasks.get(taskNumber - 1).getStatusIcon()
-                            + "] "
-                            + tasks.get(taskNumber - 1).description
-                            + "\n"
-                    );
-                } catch (IndexOutOfBoundsException | NullPointerException e) {
-                    if (tasks.isEmpty()) {
-                        ui.show("The task list is empty.\n");
-                    } else {
-                        ui.show("Invalid task index\n");
-                    }
-                } catch (NumberFormatException e) {
-                    ui.show("Non-integer task index provided\n");
-                }
-                isValid = false;
-                break;
-
-            case "unmark":
-                try {
-                    int taskNumber = Integer.parseInt(parts[1]);
-                    tasks.get(taskNumber - 1).markAsUndone();
-                    ui.show("Okay. I've marked this task as not done yet:\n"
-                            + "   ["
-                            + tasks.get(taskNumber - 1).getStatusIcon()
-                            + "] "
-                            + tasks.get(taskNumber - 1).description
-                            + "\n"
-                    );
-                } catch (IndexOutOfBoundsException | NullPointerException e) {
-                    if (tasks.isEmpty()) {
-                        ui.show("The task list is empty.\n");
-                    } else {
-                        ui.show("Invalid task index\n");
-                    }
-                } catch (NumberFormatException e) {
-                    ui.show("Non-integer task index provided\n");
-                }
-                isValid = false;
-                break;
-
-            case "delete":
-                try {
-                    int taskNumber = Integer.parseInt(parts[1]);
-                    Task removed = tasks.remove(taskNumber - 1);
-                    ui.show("Noted. I've removed this task:\n   " + removed.toString());
-
-                    ui.show("Now you have " + tasks.size() + " tasks in the list.\n");
-
-                } catch (IndexOutOfBoundsException | NullPointerException e) {
-                    if (tasks.isEmpty()) {
-                        ui.show("The task list is empty.\n");
-                    } else {
-                        ui.show("Invalid task index\n");
-                    }
-                } catch (NumberFormatException e) {
-                    ui.show("Non-integer task index provided\n");
-                }
-                isValid = false;
-                break;
-
-            case "find":
-                String[] findArr = input.split("find ");
-                isValid = false;
-                if (tasks.isEmpty()) {
-                    ui.show("The task list is empty; no tasks to be found.\n");
-                    continue;
-                }
-                String searchPhrase = findArr[1];
-                ArrayList<Task> matchingTasks = new ArrayList<>();
-
-                for (Task t: tasks.getAll()) {
-                    if (t.description.contains(searchPhrase)) {
-                        matchingTasks.add(t);
-                    }
-                }
-                if (!matchingTasks.isEmpty()) {
-                    ui.show("Here are the matching tasks in your list:");
-                    for (int j = 0; j < matchingTasks.size(); j++) {
-                        Task t = matchingTasks.get(j);
-                        ui.show((j + 1) + "." + t);
-                    }
-                } else {
-                    ui.show("Sorry, no matching tasks were found. " +
-                            "Please double check your search phrase and task list again.\n");
-                }
-                break;
-
-            case "todo":
-                try {
-                    String[] todoArr = input.split("todo ");
-                    tasks.add(new ToDo(todoArr[1]));
-                } catch (IndexOutOfBoundsException e) {
-                    ui.show("Wrong input syntax. Correct syntax is 'todo (task name)'.\n");
-                    isValid = false;
-                }
-                break;
-
-            case "deadline":
-                /* given the command deadline finish homework /by 2026-01-25 2000:
-                    deadlineArr1 = [, finish homework /by 2026-01-25 2000]
-                    deadlineArr2 = [finish homework, 2026-01-25 2000]
-                */
-                try {
-                    String[] deadlineArr1 = input.split("deadline ");
-                    String[] deadlineArr2 = deadlineArr1[1].split(" /by ");
-                    String description = deadlineArr2[0];
-                    String dateStr = deadlineArr2[1];
-
-                    LocalDateTime dateTime =
-                            LocalDateTime.parse(dateStr,
-                                    DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm"));
-
-
-                    tasks.add(new Deadline(description, dateTime));
-                } catch (IndexOutOfBoundsException | DateTimeParseException e) {
-                    ui.show("Wrong input syntax. Correct syntax is 'deadline (task name) /by (deadline)'.\n"
-                            + "Format for deadline is 'YYYY-MM-DD time'.\n"
-                            + "Times should be in 24-hour format i.e XXXX.\n"
-                    );
-                    isValid = false;
-                }
-                break;
-
-            case "event":
-                /* given the command event family dinner /from 2026-01-25 6pm /to 2026-01-25 7pm:
-                    eventArr1 = [, family dinner /from 2026-01-25 6pm /to 2026-01-25 7pm]
-                    eventArr2 = [family dinner, 2026-01-25 6pm /to 2026-01-25 7pm]
-                    eventArr3 = [2026-01-25 6pm, 2026-01-25 7pm]
-                */
-                try {
-                    String[] eventArr1 = input.split("event ");
-                    String[] eventArr2 = eventArr1[1].split(" /from ");
-                    String[] eventArr3 = eventArr2[1].split(" /to ");
-
-                    String fromDateStr = eventArr3[0];
-                    String toDateStr = eventArr3[1];
-
-                    LocalDateTime fromDateTime =
-                            LocalDateTime.parse(fromDateStr,
-                                    DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm"));
-
-                    LocalDateTime toDateTime =
-                            LocalDateTime.parse(toDateStr,
-                                    DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm"));
-
-                    tasks.add(new Event(eventArr2[0], fromDateTime, toDateTime));
-                } catch (IndexOutOfBoundsException | DateTimeParseException e) {
-                    ui.show("Wrong input syntax. Correct syntax is 'event (task name) /from (start) /to (end)'.\n"
-                            + "Format for both start and end is 'YYYY-MM-DD time'.\n"
-                            + "Times should be in 24-hour format i.e XXXX.\n"
-                    );
-                    isValid = false;
-                }
-                break;
-
-            case "invalid":
-                // invalid command or empty input
-                ui.show("Your input is either empty or not recognised. Please try again.\n");
-                isValid = false;
-                break;
-
-            default:
-                break;
-            }
-
-
-            if (isValid) {
-                // only increments and adds to task list if task command is valid
-                ui.show("\n" + "Got it. I've added this task: " + tasks.get(tasks.size() - 1).toString() + "\n");
-                ui.show("Now you have " + tasks.size() + " tasks in the list.\n");
-            }
-
+        case "mark":
             try {
-                storage.save(tasks.getAll());
-            } catch (Exception e) {
-                ui.show("Error saving tasks.");
+                int taskNumber = Integer.parseInt(parts[1]);
+                tasks.get(taskNumber - 1).markAsDone();
+                response = "Okay. I've marked this task as done:\n"
+                        + "   ["
+                        + tasks.get(taskNumber - 1).getStatusIcon()
+                        + "] "
+                        + tasks.get(taskNumber - 1).description
+                        + "\n";
+            } catch (IndexOutOfBoundsException | NullPointerException e) {
+                if (tasks.isEmpty()) {
+                    response = "The task list is empty.\n";
+                } else {
+                    response = "Invalid task index\n";
+                }
+            } catch (NumberFormatException e) {
+                response = "Non-integer task index provided\n";
             }
+            isValid = false;
+            break;
+
+        case "unmark":
+            try {
+                int taskNumber = Integer.parseInt(parts[1]);
+                tasks.get(taskNumber - 1).markAsUndone();
+                response = "Okay. I've marked this task as not done yet:\n"
+                        + "   ["
+                        + tasks.get(taskNumber - 1).getStatusIcon()
+                        + "] "
+                        + tasks.get(taskNumber - 1).description
+                        + "\n";
+            } catch (IndexOutOfBoundsException | NullPointerException e) {
+                if (tasks.isEmpty()) {
+                    response = "The task list is empty.\n";
+                } else {
+                    response = "Invalid task index\n";
+                }
+            } catch (NumberFormatException e) {
+                response = "Non-integer task index provided\n";
+            }
+            isValid = false;
+            break;
+
+        case "delete":
+            try {
+                int taskNumber = Integer.parseInt(parts[1]);
+                Task removed = tasks.remove(taskNumber - 1);
+                response = "Noted. I've removed this task:\n   " + removed.toString() + "\n";
+
+                response += "Now you have " + tasks.size() + " tasks in the list.\n";
+
+            } catch (IndexOutOfBoundsException | NullPointerException e) {
+                if (tasks.isEmpty()) {
+                    response = "The task list is empty.\n";
+                } else {
+                    response = "Invalid task index\n";
+                }
+            } catch (NumberFormatException e) {
+                response = "Non-integer task index provided\n";
+            }
+            isValid = false;
+            break;
+
+        case "find":
+            String[] findArr = input.split("find ");
+            isValid = false;
+            if (tasks.isEmpty()) {
+                response = "The task list is empty; no tasks to be found.\n";
+            }
+            String searchPhrase = findArr[1];
+            ArrayList<Task> matchingTasks = new ArrayList<>();
+
+            for (Task t: tasks.getAll()) {
+                if (t.description.contains(searchPhrase)) {
+                    matchingTasks.add(t);
+                }
+            }
+            if (!matchingTasks.isEmpty()) {
+                response = "Here are the matching tasks in your list:\n";
+                for (int j = 0; j < matchingTasks.size(); j++) {
+                    Task t = matchingTasks.get(j);
+                    response += (j + 1) + "." + t;
+                }
+            } else {
+                response = "Sorry, no matching tasks were found. " +
+                        "Please double check your search phrase and task list again.\n";
+            }
+            break;
+
+        case "todo":
+            try {
+                String[] todoArr = input.split("todo ");
+                tasks.add(new ToDo(todoArr[1]));
+            } catch (IndexOutOfBoundsException e) {
+                response = "Wrong input syntax. Correct syntax is 'todo (task name)'.\n";
+                isValid = false;
+            }
+            break;
+
+        case "deadline":
+            /* given the command deadline finish homework /by 2026-01-25 2000:
+                deadlineArr1 = [, finish homework /by 2026-01-25 2000]
+                deadlineArr2 = [finish homework, 2026-01-25 2000]
+            */
+            try {
+                String[] deadlineArr1 = input.split("deadline ");
+                String[] deadlineArr2 = deadlineArr1[1].split(" /by ");
+                String description = deadlineArr2[0];
+                String dateStr = deadlineArr2[1];
+
+                LocalDateTime dateTime =
+                        LocalDateTime.parse(dateStr,
+                                DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm"));
+
+
+                tasks.add(new Deadline(description, dateTime));
+            } catch (IndexOutOfBoundsException | DateTimeParseException e) {
+                response = "Wrong input syntax. Correct syntax is 'deadline (task name) /by (deadline)'.\n"
+                        + "Format for deadline is 'YYYY-MM-DD time'.\n"
+                        + "Times should be in 24-hour format i.e XXXX.\n";
+                isValid = false;
+            }
+            break;
+
+        case "event":
+            /* given the command event family dinner /from 2026-01-25 6pm /to 2026-01-25 7pm:
+                eventArr1 = [, family dinner /from 2026-01-25 6pm /to 2026-01-25 7pm]
+                eventArr2 = [family dinner, 2026-01-25 6pm /to 2026-01-25 7pm]
+                eventArr3 = [2026-01-25 6pm, 2026-01-25 7pm]
+            */
+            try {
+                String[] eventArr1 = input.split("event ");
+                String[] eventArr2 = eventArr1[1].split(" /from ");
+                String[] eventArr3 = eventArr2[1].split(" /to ");
+
+                String fromDateStr = eventArr3[0];
+                String toDateStr = eventArr3[1];
+
+                LocalDateTime fromDateTime =
+                        LocalDateTime.parse(fromDateStr,
+                                DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm"));
+
+                LocalDateTime toDateTime =
+                        LocalDateTime.parse(toDateStr,
+                                DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm"));
+
+                tasks.add(new Event(eventArr2[0], fromDateTime, toDateTime));
+            } catch (IndexOutOfBoundsException | DateTimeParseException e) {
+                response = "Wrong input syntax. Correct syntax is 'event (task name) /from (start) /to (end)'.\n"
+                        + "Format for both start and end is 'YYYY-MM-DD time'.\n"
+                        + "Times should be in 24-hour format i.e XXXX.\n";
+                isValid = false;
+            }
+            break;
+
+        case "invalid":
+            // invalid command or empty input
+            response = "Your input is either empty or not recognised. Please try again.\n";
+            isValid = false;
+            break;
+
+        default:
+            break;
         }
+
+        if (isValid) {
+            // only increments and adds to task list if task command is valid
+            response = "\n" + "Got it. I've added this task: " + tasks.get(tasks.size() - 1).toString() + "\n";
+            response += "Now you have " + tasks.size() + " tasks in the list.\n";
+        }
+
+        try {
+            storage.save(tasks.getAll());
+        } catch (Exception e) {
+            response = "Error saving tasks.";
+        }
+        return response;
     }
 
-    /**
-     * Executes the run method at the specific file path.
-     */
-    public static void main(String[] args) {
-        new Shiho("data/tasks.txt").run();
+    public String getStartupMessage() {
+        return startupMessage;
     }
 }
 
